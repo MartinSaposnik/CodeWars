6b65ec83a60fbfed521d80c010a7dac709365151
MartinSaposnik <martinrobertosaposnik@gmail.com>
2022-10-28 03:18:07 -0300

    add You can't code under pressure #1

---
+++ 8kyu/You-Canot-Code-Under-Pressure/YouCanotCodeUnderPressure.js
@@ -0,0 +1,10 @@
+function doubleInteger(i) {
+    // i will be an integer. Double it and return it.
+    let dup = i * 2;
+    return dup;
+}
+
+/* function doubleInteger(i) {
+    // i will be an integer. Double it and return it.
+    return i * 2;
+} */
