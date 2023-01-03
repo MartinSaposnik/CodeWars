b8879ad1e51aa145bf7c00ef6daf2293355ff1fb
Martin Saposnik <martinrobertosaposnik@gmail.com>
2023-01-03 03:29:14 -0300

    add invert values

---
+++ 8kyu/Invert-Values/InvertValues.js
@@ -0,0 +1,11 @@
+function invert(array) {
+    return array.map((n)=> n * -1) ;
+}
+
+/* function invert(array) {
+    var newArr = [];
+    for (var i = 0; i < array.length; i++) {
+        newArr.push(-array[i]);
+    }
+    return newArr;
+} */
